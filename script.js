66c70162f7c4d2b3232df74bf9394e0f0aecc8c7
Ignacio Chaneton <chata@MacBook-Pro-de-Ignacio.local>
2024-12-13 20:50:48 -0300

    1.5.9(resp5)

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.addEventListener("load",function() {
-    setTimeout(function(){
-        // This hides the address bar:
-        window.scrollTo(0, 500);
-    }, 0);
-});
+// document.addEventListener("load",function() {
+//     setTimeout(function(){
+//         // This hides the address bar:
+//         window.scrollTo(0, 500);
+//     }, 0);
+// });
 
 const main_container = document.getElementById('main-container');
 const ambiente = document.getElementById('ambiente');
